2d03b4ac4d841b8c768f22efbc003239ad7dc38b
xunit00 <xunit00@gmail.com>
2019-06-23 20:14:05 -0400

    arreglar chceckbox

--- resources/views/layouts/permisos.blade.php
+++ resources/views/layouts/permisos.blade.php
@@ -3,10 +3,10 @@
         <label class="col-md-2">Permisos</label>
         <div class="col-md-6">
 
-            <div class="custom-control custom-switch my-1 mr-sm-2">
-                <input type="checkbox" class="custom-control-input" id="customCheck"  onClick="selectAll(this)">
-                <label class="custom-control-label" for="customCheck">Seleccionar Todo</label>
-              </div>
+            <div class="form-check my-1 mr-sm-2">
+                <input type="checkbox" class="form-check-input" onClick="selectAll(this)">
+                <label class="form-check-label">Seleccionar Todo</label>
+            </div>
 
         </div>
     </div>
@@ -16,16 +16,24 @@
         <section class="content">
             <div class="container-fluid mt-2">
 
-                @foreach($permissions as $permission=>$all)
+                {{-- @foreach($permissions as $permission=>$all)
                 <div class="custom-control custom-switch my-1 mr-sm-2">
-                    <input type="checkbox" class="custom-control-input" id="customCheck"
-                    onClick="selectAll(this)" @error('permission') is-invalid @enderror
-                    name="permission[]" value="{{$all}}" @foreach($my_perm as $mp=>$value)
-                    @if($all==$value) checked @endif  @endforeach>
-                    <label class="custom-control-label" for="customCheck">{{$all}}</label>
-                  </div>
+                    <input type="checkbox" class="custom-control-input" id="permission[]" @error('permission')
+                        is-invalid @enderror name="permission[]" value="{{$all}}" @foreach($my_perm as $mp=>$value)
+                    @if($all==$value) checked @endif @endforeach>
+                    <label class="custom-control-label" for="permission[]">{{$all}}</label>
+                </div>
+                @endforeach --}}
 
+                @foreach($permissions as $permission=>$all)
+                <div class="form-check my-1 mr-sm-2">
+                    <input type="checkbox" class="form-check-input" @error('permission')
+                        is-invalid @enderror name="permission[]" value="{{$all}}" @foreach($my_perm as $mp=>$value)
+                    @if($all==$value) checked @endif @endforeach>
+                    <label class="form-check-label">{{$all}}</label>
+                </div>
                 @endforeach
+
             </div>
         </section>
     </div>
