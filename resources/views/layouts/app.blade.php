f0c5ae8cb9b0cf8c2df9f4a0caedbd11b538ab96
xunit00 <xunit00@gmail.com>
2019-06-24 19:38:24 -0400

    sin adminlte

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
     <!-- Ionicons -->
     <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
     <!-- Theme style -->
-    <link rel="stylesheet" href="{{asset('admin-lte/dist/css/adminlte.min.css')}}">
+    {{-- <link rel="stylesheet" href="{{asset('admin-lte/dist/css/adminlte.min.css')}}"> --}}
     <!-- Google Font: Source Sans Pro -->
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 </head>
