563d56c35215c21b11e5d4ddd6e0bd759ef3616f
xunit00 <xunit00@gmail.com>
2019-07-22 15:39:12 -0400

    mostrando prod unidades en index

--- resources/views/productos/index.blade.php
+++ resources/views/productos/index.blade.php
@@ -7,9 +7,10 @@
             <div class="col-sm-6">
 
                 <ul class="nav nav-pills ml-auto p-2">
-                    <li class="nav-item"><a class="nav-link active show" href="#tab_1" data-toggle="tab">Productos</a>
+                    <li class="nav-item"><a class="nav-link " href="#tab_1" data-toggle="tab">Productos</a>
                     </li>
-                    <li class="nav-item"><a class="nav-link" href="#tab_2" data-toggle="tab">Unidad Productos</a></li>
+                    <li class="nav-item"><a class="nav-link active show" href="#tab_2" data-toggle="tab">Unidad
+                            Productos</a></li>
                 </ul>
             </div><!-- /.col -->
 
@@ -28,14 +29,14 @@
     <div class="card-header">
         <div class="row">
             <div class="col-md-10">
-                    @if (session('success'))
-                    <div class="alert alert-success alert-dismissible fade show mt-2" role="alert">
-                        <p>{{ session('success') }}</p>
-                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    @endif
+                @if (session('success'))
+                <div class="alert alert-success alert-dismissible fade show mt-2" role="alert">
+                    <p>{{ session('success') }}</p>
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                @endif
             </div>
             <div class="col-md-2">
                 <div class="card-tools ">
@@ -55,12 +56,12 @@
 
     <div class="card-body">
         <div class="tab-content">
-            <div class="tab-pane active show" id="tab_1">
+            <div class="tab-pane" id="tab_1">
 
-        <a class="btn btn-success" href="{{route('productos.create')}}">
-        Crear Producto
-        <i class="fa fa-th-large"></i>
-        </a>
+                <a class="btn btn-success" href="{{route('productos.create')}}">
+                    Crear Producto
+                    <i class="fa fa-th-large"></i>
+                </a>
 
                 <div class="card-body table-responsive p-0 mt-3">
                     <table class="table table-hover">
@@ -104,61 +105,64 @@
 
             </div>
             <!-- /.tab-pane -->
-            <div class="tab-pane" id="tab_2">
+            <div class="tab-pane active show" id="tab_2">
 
 
-                    <a class="btn btn-success" href="{{route('productos.create_produnid')}}">
-                            Crear Unidad/Producto
-                            <i class="fa fa-th-large"></i>
-                            </a>
+                <a class="btn btn-success" href="{{route('productos.create_produnid')}}">
+                    Crear Unidad/Producto
+                    <i class="fa fa-th-large"></i>
+                </a>
 
-                                    <div class="card-body table-responsive p-0 mt-3">
-                                        <table class="table table-hover">
-                                            <tbody>
-                                                <tr>
-                                                    <th>Producto</th>
-                                                    <th>Unidad</th>
-                                                    <th>Cantidad</th>
-                                                    <th>Precio Venta</th>
-                                                    <th>Costo</th>
-                                                    <th>Modyfy</th>
-                                                </tr>
-                                                @foreach($prod_unidad as $p_unid)
-                                                <tr>
-                                                {{-- <td>{{}}</td> --}}
-                                                    {{-- <td>{{$p_unid->nombre_unidad}}</td>
-                                                    <td>{{$p_unid->productos->pivot->cantidad}}</td>
-                                                    <td>{{$p_unid->productos->pivot->precio_venta}}</td>
-                                                    <td>{{$p_unid->productos->pivot->costo}}</td> --}}
-                                                    <td>
-                                                        <form action="{{route('productos.destroy',$prod->id)}}" method="POST">
-                                                            @can('update.role')
-                                                            <a class="btn btn-outline-secondary btn-sm"
-                                                                href="{{route('productos.edit',$prod->id)}}">
-                                                                <i class="fa fa-edit"></i>
-                                                            </a>
-                                                            @endcan
+                <div class="card-body table-responsive p-0 mt-3">
+                    <table class="table table-hover">
+                        <tbody>
+                            <tr>
+                                <th>Producto</th>
+                                <th>Unidad</th>
+                                <th>Cantidad</th>
+                                <th>Precio Venta</th>
+                                <th>Costo</th>
+                                <th>Modyfy</th>
+                            </tr>
+                            @foreach($prod_unidad as $p_unid)
+                                @foreach($p_unid->productos as $p_u)
+                                    <tr>
+                                        {{-- <td>{{$p_u}}</td> --}}
+                                        <td>{{$p_u->nombre_producto}}</td>
+                                        <td>{{$p_unid->nombre_unidad}}</td>
+                                        <td>{{$p_u->pivot->cantidad}}</td>
+                                        <td>{{$p_u->pivot->precio_venta}}</td>
+                                        <td>{{$p_u->pivot->costo}}</td>
+                                        <td>
+                                            <form action="{{route('productos.destroy',$prod->id)}}" method="POST">
+                                                @can('update.role')
+                                                <a class="btn btn-outline-secondary btn-sm"
+                                                    href="{{route('productos.edit',$prod->id)}}">
+                                                    <i class="fa fa-edit"></i>
+                                                </a>
+                                                @endcan
 
-                                                            @csrf
-                                                            @method('DELETE')
-                                                            @can('delete.users')
-                                                            <button type="submit" class="btn btn-outline-danger btn-sm"
-                                                                onclick="return confirm('Quiere Borrar este Registro?')">
-                                                                <i class="fas fa-trash-alt"></i></button>
-                                                            @endcan
-                                                        </form>
-                                                    </td>
-                                                </tr>
-                                                @endforeach
-                                            </tbody>
-                                        </table>
-                                        {{$productos->links()}}
+                                                @csrf
+                                                @method('DELETE')
+                                                @can('delete.users')
+                                                <button type="submit" class="btn btn-outline-danger btn-sm"
+                                                    onclick="return confirm('Quiere Borrar este Registro?')">
+                                                    <i class="fas fa-trash-alt"></i></button>
+                                                @endcan
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            @endforeach
+                        </tbody>
+                    </table>
+                    {{$prod_unidad->links()}}
 
+                </div>
             </div>
-        </div>
-        <!-- /.tab-content -->
-    </div><!-- /.card-body -->
-</div>
-<!-- /.card-header -->
+            <!-- /.tab-content -->
+        </div><!-- /.card-body -->
+    </div>
+    <!-- /.card-header -->
 </div>
 @endsection
