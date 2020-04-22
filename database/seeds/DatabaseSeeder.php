cc23c525ad928a70193e4605193c849c1b56cb73
mbaret <mbaret@jqmicro.com>
2020-04-22 14:52:28 -0400

    preparaciones fix

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -21,7 +21,7 @@ class DatabaseSeeder extends Seeder
             AreasSeed::class,
             MesasSeed::class,
             InsumoSeed::class,
-            PreparacionesInsumosSeed::class,
+            PreparacionSeed::class,
             PlatosSeed::class,
         ]);
     }
--- database/seeds/PreparacionSeed.php
+++ database/seeds/PreparacionSeed.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Seeder;
 use App\Preparacion;
 
-class PreparacionesInsumosSeed extends Seeder
+class PreparacionSeed extends Seeder
 {
     /**
      * Run the database seeds.
